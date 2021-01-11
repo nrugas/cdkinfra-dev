39da846d492879be8ae71a51b3d7e4d436cf0556
nrugas <nevioruas@gmail.com>
2021-01-11 12:19:05 +0100

    dodao lambdu

--- cdkinfra_dev/cdkinfra_dev_stack.py
+++ cdkinfra_dev/cdkinfra_dev_stack.py
@@ -1,9 +1,6 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+
 )
 
 
@@ -12,13 +9,7 @@ class CdkinfraDevStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkinfraDevQueue",
-            visibility_timeout=core.Duration.seconds(300),
-        )
+        
 
-        topic = sns.Topic(
-            self, "CdkinfraDevTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        #VPC
+        vpc = ec2.Vpc(self, "CDK_VPC")
